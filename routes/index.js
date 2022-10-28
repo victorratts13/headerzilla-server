b2dd2ddddc054cbbe63d80edac0abae22696673f
Victor Ratts <victor@roverlabs.com.br>
2022-10-28 17:17:29 -0300

    update

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ var { SetHeaderKey } = require('../controller/ServerKeyController.js');
 var url = 'https://www.ifood.com.br/delivery/fortaleza-ce/mais-acai-mania-parque-dois-irmaos/7a0f1bd4-5673-460f-8269-c0bbed381547';
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    'service': 'running',
+    'status': true
+  })
 });
 
 router.get('/serverkey', async function (req, res, next) {
